31b61fc46fc36047f1ce5f6a2dd810ac3275ab18
Jay Patel <49103665+MR-JAYPATEL-529@users.noreply.github.com>
2024-02-18 04:36:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 $(document).ready(function() {
-  $(".click-effect").click(function() {
-    $(this).animate({ marginLeft: "50px" }, "slow");
+    $(".click-effect").click(function() {
+      $(this).animate({ marginLeft: "50px" }, "slow");
+    });
   });
-});
+
+window.addEventListener('deviceorientation', handleOrientation);
+
+function handleOrientation(event) {
+    var beta = event.beta; // Rotation around the front-to-back axis
+    var gamma = event.gamma; // Rotation around the left-to-right axis
+
+    // Apply the rotation to your card
+    document.getElementById('card-container').style.transform = 
+        'rotateX(' + beta + 'deg) rotateY(' + gamma + 'deg)';
+}
 
